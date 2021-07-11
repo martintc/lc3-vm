e21447a3e0263903af70a8dea48d42e63b26d572
martintc <kj4ntv@gmail.com>
2021-07-11 21:07:18 +0000

    Implement function to read in program to memory

--- src/lc3.c
+++ src/lc3.c
@@ -66,6 +66,23 @@ enum {
   FL_NEG = 1 << 2, // negative
 };
 
+// read program into memory
+void read_image_file(FILE* file) {
+  uint16_t origin;
+  fread(&origin, sizeof(origin), 1, file);
+  origin = swap16(origin);
+
+  uint16_t max_read = UINT16_MAX - origin;
+  uint16_t* p = memory + origin;
+  size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+  // swap to little endian
+  while (read --> 0) {
+    *p = swap16(*p);
+    ++p;
+  }
+}
+
 // setting the condition register using the condition flags
 void update_flags(uint16_t r) {
   if(reg[r] == 0) {
