e6caeb08a1d70ebe08f290d228c585be19ee43ce
Todd Martin <todd@bsdppc.Home>
2021-07-11 20:03:28 -0700

    Initial file dump of new files

---
+++ src/lc3_util.h
@@ -0,0 +1,8 @@
+#ifndef LC3_UTIL_H
+#define LC3_UTIL_H
+
+#include <stdint.h>
+
+uint16_t sign_extend(uint16_t x, int bit_count);
+
+#endif
---
+++ src/ops.h
@@ -0,0 +1,10 @@
+#ifndefine OP_H
+#define OP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+
+#endif
